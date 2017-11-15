33bc5599bfd1f4ddc85a33ecb8bcaff197648f20
Anders T. Akre <anders.takre@gmail.com>
2017-11-15 09:14:00 +0100

    Install Vim-Plug

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 ## Neovim
+# Install vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 mkdir -p ~/.config/nvim
 ln -s $(realpath vimrc) ~/.config/nvim/init.vim
