a6ee3918c2430acd959513fef3590982352fdd74
Anders T. Akre <anders.takre@gmail.com>
2018-09-16 22:28:31 +0200

    Add Alacritty, i3 and polybar configurations

--- install.sh
+++ install.sh
@@ -5,5 +5,10 @@
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-mkdir -p ~/.config/nvim
+mkdir -p ~/.config/{nvim,i3,alacritty,polybar}
+
 ln -s $(realpath vimrc) ~/.config/nvim/init.vim
+ln -s $(realpath i3) ~/.config/i3/config
+ln -s $(realpath alacritty.yml) ~/.config/alacritty/alacritty.yml
+ln -s $(realpath polybar) ~/.config/polybar/config
+ln -s $(realpath launch_polybar.sh) ~/.config/polybar/launch_polybar.sh
