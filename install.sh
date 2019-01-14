16cf3f2e643ca2af83c7abe7121532a389dcf97c
Anders T. Akre <anders.akre@sonitor.com>
2019-01-14 11:24:25 +0100

    Install vimrc to vim default location

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-## Neovim
-# Install vim-plug
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-mkdir -p ~/.config/{nvim,i3,alacritty,polybar}
+mkdir -p ~/.config/{i3,alacritty,polybar}
 
-ln -s $(realpath vimrc) ~/.config/nvim/init.vim
+ln -s $(realpath vimrc) ~/.vimrc
 ln -s $(realpath i3) ~/.config/i3/config
 ln -s $(realpath alacritty.yml) ~/.config/alacritty/alacritty.yml
 ln -s $(realpath polybar) ~/.config/polybar/config
