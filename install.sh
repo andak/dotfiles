cabb6f8449239b5927389193c43af6f0741aaacb
Anders T. Akre <anders.akre@sonitor.com>
2019-11-03 20:46:57 +0100

    Add nerdfonts

--- install.sh
+++ install.sh
@@ -11,3 +11,9 @@ ln -s $(realpath i3) ~/.config/i3/config
 ln -s $(realpath alacritty.yml) ~/.config/alacritty/alacritty.yml
 ln -s $(realpath polybar) ~/.config/polybar/config
 ln -s $(realpath launch_polybar.sh) ~/.config/polybar/launch_polybar.sh
+
+mkdir ~/repo
+# Must-have programming fonts
+git clone https://github.com/ryanoasis/nerd-fonts.git ~/repo/nerd-fonts
+
+# Also check out: https://app.programmingfonts.org/
