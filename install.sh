f5b0070e622a7e3098c2f0dfd537b40dad439287
Anders T. Akre <anders.akre@heimdallpower.com>
2023-07-26 13:12:23 +0200

    Remove polybar from install script

--- install.sh
+++ install.sh
@@ -4,16 +4,13 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-mkdir -p ~/.config/{i3,alacritty,polybar}
+mkdir -p ~/.config/{i3,alacritty}
 
 ln -s $(realpath vimrc) ~/.vimrc
 ln -s $(realpath i3) ~/.config/i3/config
 ln -s $(realpath alacritty.yml) ~/.config/alacritty/alacritty.yml
-ln -s $(realpath polybar) ~/.config/polybar/config
-ln -s $(realpath launch_polybar.sh) ~/.config/polybar/launch_polybar.sh
 
 mkdir -p ~/repo
 # Must-have programming fonts
 git clone https://github.com/ryanoasis/nerd-fonts.git ~/repo/nerd-fonts
-
 # Also check out: https://app.programmingfonts.org/
